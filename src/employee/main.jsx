e0e6be1464cca24bd2899341fab4f11454a41707
uneechung-lab <unee.chung@gmail.com>
2026-04-07 12:15:37 +0900

    fix: main.jsx 100vh 직접 설정

--- src/employee/main.jsx
+++ src/employee/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootEl = document.getElementById('root')
+rootEl.style.height = '100vh'
+rootEl.style.width = '100vw'
+rootEl.style.display = 'flex'
+rootEl.style.overflow = 'hidden'
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode><App /></React.StrictMode>
 )
