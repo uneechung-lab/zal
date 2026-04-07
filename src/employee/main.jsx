761d0d8e92068891eb13fce0bcb14417c316e05b
uneechung-lab <unee.chung@gmail.com>
2026-04-07 12:33:54 +0900

    fix: CSS 초기화

--- src/employee/main.jsx
+++ src/employee/main.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
+﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const rootEl = document.getElementById('root')
-rootEl.style.height = '100vh'
-rootEl.style.width = '100vw'
-rootEl.style.display = 'flex'
-rootEl.style.overflow = 'hidden'
-
-ReactDOM.createRoot(rootEl).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
 )
